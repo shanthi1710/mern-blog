1a29f648db3622db76af527de1b67d45cc3325ca
Shanthinath Shedabale <shanthinathshedabale17@gmail.com>
2024-05-08 13:31:18 +0530

    feat: Add getUsers API route and controller function

--- api/src/routes/user.route.js
+++ api/src/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import {deleteUser, signOut,test,updateUser} from "../controllers/user.controller.js";
+import {deleteUser, signOut,test,updateUser,getUsers} from "../controllers/user.controller.js";
 import {verifyToken} from "../middlewares/auth.middleware.js";
 const router = express.Router();
 
 router.get("/test",test);
 router.put("/update/:userId",verifyToken,updateUser);
-router.delete("/delete/:userId",verifyToken,deleteUser)
-router.post('/signout',verifyToken,signOut)
+router.delete("/delete/:userId",verifyToken,deleteUser);
+router.post('/signout',verifyToken,signOut);
+router.get('/getusers', verifyToken, getUsers);
 export default router;
