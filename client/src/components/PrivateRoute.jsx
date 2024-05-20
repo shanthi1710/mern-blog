67419b2ee333dd468de08b0139da578f8375a7ba
shathi1710 <er.shathi123@gmail.com>
2024-05-20 20:56:55 +0530

    remove error

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -1,11 +1,7 @@
-import { useSelector } from "react-redux";
-import { Outlet, Navigate } from "react-router-dom";
+import { useSelector } from 'react-redux';
+import { Outlet, Navigate } from 'react-router-dom';
 
-export default function OnlyAdminPrivateRoute() {
+export default function PrivateRoute() {
   const { currentUser } = useSelector((state) => state.user);
-  return currentUser && currentUser.isAdmin ? (
-    <Outlet />
-  ) : (
-    <Navigate to="/sign-in" />
-  );
+  return currentUser ? <Outlet /> : <Navigate to='/sign-in' />;
 }
